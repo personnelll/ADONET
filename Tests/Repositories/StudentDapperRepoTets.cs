c87d593e98d35e6f5d0d8b8152fa85310eba8f16
Quentin-Latupia <quentinplatiau@gmail.com>
2026-05-15 20:40:59 +0200

    un instance par fichier de tests

--- Tests/Repositories/StudentDapperRepoTets.cs
+++ Tests/Repositories/StudentDapperRepoTets.cs
@@ -1,53 +1,38 @@
-﻿using Castle.Core.Logging;
-using DotNet.Testcontainers.Builders;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Logging.Abstractions;
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
 using Repositories;
 using Shared;
 using Testcontainers.MsSql;
 
 namespace Tests.RepositoriesTests
 {
-    public class StudentDapperRepoTets
+    public class StudentDapperRepoTets : IClassFixture<DatabaseFixture>
     {
-        private MsSqlContainer _container;
+        private readonly DatabaseFixture _fixture;
+        private string _connectionString;
+        DBSetup _dbSetup => _fixture.DbSetup;
+
+        public StudentDapperRepoTets(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+            _connectionString = fixture.ConnectionString;
+
+        }
 
         [Fact]
         public async Task GetAllTest()
         {
-            var container = new MsSqlBuilder()
-                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-                .WithPassword("Your_strong(!)Password")
-                .Build();
+            await _dbSetup.InitStudentsDataAsync();           
 
-            await container.StartAsync();
+            var logger = NullLogger<StudentDapperRepo>.Instance;
+            var repo = new StudentDapperRepo(logger, _connectionString);
 
-            DBSetup dbSetup = new DBSetup(container.GetConnectionString());
+            var students = repo.GetAll();
 
-            await dbSetup.CreatDBAsync();
-
-            await dbSetup.CreatTablesAsync();
-
-            await dbSetup.InitStudentsDataAsync();
-
-            try
-            {
-                string connectionString = container.GetConnectionString();
-                connectionString.Replace("master;", "CoursSGBD");
-
-                var logger = NullLogger<StudentDapperRepo>.Instance;
-                var repo = new StudentDapperRepo(logger, connectionString);
-
-                var students = repo.GetAll();
-
-                Assert.NotNull(students);
-                Assert.NotEmpty(students);
-                Assert.Equal(3, students.Count);
-            }
-            finally
-            {
-                await container.DisposeAsync();
-            }
+            Assert.NotNull(students);
+            Assert.NotEmpty(students);
+            Assert.Equal(3, students.Count);
+            
         }
 
         [Theory]
@@ -56,40 +41,17 @@ namespace Tests.RepositoriesTests
         [InlineData("Pironet", 1)]
         public async Task FindStudentsByLastName(string search, int result)
         {
-            var container = new MsSqlBuilder()
-                .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-                .WithPassword("Your_strong(!)Password")
-                .Build();
+            await _dbSetup.InitStudentsDataAsync();
 
-            await container.StartAsync();
+            // instantiate repo with NullLogger and injected connection string
+            var logger = NullLogger<Repositories.StudentDapperRepo>.Instance;
+            var repo = new Repositories.StudentDapperRepo(logger, _connectionString);
 
-            DBSetup dbSetup = new DBSetup(container.GetConnectionString());
+            // act
+            var students = repo.GetByLastName(search);
 
-            await dbSetup.CreatDBAsync();
+            Assert.Equal(result, students.Count);
 
-            await dbSetup.CreatTablesAsync();
-
-            await dbSetup.InitStudentsDataAsync();
-
-            try
-            {
-                string connectionString = container.GetConnectionString();
-                connectionString.Replace("master;", "CoursSGBD");
-
-                var logger = NullLogger<StudentDapperRepo>.Instance;
-                var repo = new StudentDapperRepo(logger, connectionString);
-
-                //act
-                var students = repo.GetByLastName(search);
-                //assert
-                Assert.NotNull(students);
-                Assert.NotEmpty(students);
-                Assert.Equal(result, students.Count);
-            }
-            finally
-            {
-                await container.DisposeAsync();
-            }
         }
     }
 }
